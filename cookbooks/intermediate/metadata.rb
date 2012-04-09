c8b6b5bad7f8064319ca6878dcab9966f54823a5
deanonishi <dean@rightscale.com>
2012-04-09 21:40:55 +0000

    Fixed typo in metadata

--- cookbooks/intermediate/metadata.rb
+++ cookbooks/intermediate/metadata.rb
@@ -8,12 +8,12 @@ version          "0.0.1"
 depends "rs_utils"
 
 recipe "intermediate::default","Prints hello world output" 
-recipe "intermediate::my_first_reipe","Prints hello world output"
+recipe "intermediate::my_first_recipe","Prints hello world output"
 
-attribute "<intermediate>/first_name",
+attribute "intermediate/first_name",
   :display_name => "First Name",
   :description => "Your given first name.",
   :required => 'required',
   :type => "hash",
   :default => "YOUR NAME HERE",
-  :recipes => ["<intermediate>::my_first_recipe"]
+  :recipes => ["intermediate::my_first_recipe"]
