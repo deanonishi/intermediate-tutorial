f7774825b7373d26f3cb11c461b453616a1a00fb
deanonishi <dean@rightscale.com>
2012-04-11 23:33:45 +0000

    Added inputs

--- cookbooks/about_me/metadata.rb
+++ cookbooks/about_me/metadata.rb
@@ -8,5 +8,19 @@ version          "0.0.1"
 depends "rs_utils"
 
 recipe "about_me::default","Prints my first name as output"
+recipe "about_me::my_city","Information about my city"
 
+attribute "about_me/city_name",
+   :display_name => "City Name", 
+   :description => "The city where I currently live.",
+   :type => "array",
+   :required => "required",
+   :recipes => ["about_me::my_city"]
+
+attribute "about_me/temperature",
+   :display_name => "Temperature", 
+   :description => "Current temperature in the city",
+   :type => "hash",
+   :required => "optional",
+   :recipes => ["about_me::my_city"]
 
