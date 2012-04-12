1adf47a4b358729360439501080ed187559dd1eb
deanonishi <dean@rightscale.com>
2012-04-12 00:18:56 +0000

    updates

--- cookbooks/about_me/recipes/my_city.rb
+++ cookbooks/about_me/recipes/my_city.rb
@@ -3,7 +3,6 @@ rs_utils_marker :begin
   city_name = node[:about_me][:city_name]
   temperature = node[:about_me][:temperature]
       
-  log "I live in #{city_name}",
-  log "The current temperature is #{temperature} degrees."
+  log "I live in #{city_name}. The current temperature is #{temperature} degrees."
             
 rs_utils_marker :end
